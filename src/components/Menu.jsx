b61abbed3d7282efee927f666bfe8f9129aabd24
Mykhailo-Zhuk <mishazhuk1@gmail.com>
2025-01-03 23:17:34 +0200

    fix adding book to cart

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,28 +3,35 @@ import React from "react";
 import "../index.css";
 import Logo from "../assets/images2.png";
 import { Link } from "react-router-dom";
+import ShopCard from "./ShopCard";
 const MenuList = [
-  "The CLUB",
-  "New products",
-  "Authors",
-  "Видавництва",
-  "TOP books",
-  [<Link to={"/shop"}>Your Car </Link>],
-  [<Link to={"/signUp"}>Sign Up</Link>],
-  "Blog",
-  "Shops and contacts",
-  "Delivery and payment",
+  { id: 1, title: "The CLUB", url: "#" },
+  { id: 2, title: "New products", url: "#" },
+  { id: 3, title: "Authors", url: "#" },
+  { id: 4, title: "Видавництва", url: "#" },
+  { id: 5, title: "TOP books", url: "#" },
+  { id: 7, title: "Sign Up", url: "#" },
+  { id: 8, title: "Blog", url: "#" },
+  { id: 9, title: "Shops and contacts", url: "#" },
+  { id: 10, title: "Delivery and payment", url: "#" },
 ];
 const PhoneContact = ["Online shop", "(0800) 33-05-66", "(044) 333-42-20"];
 const Menu = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
   return (
-    <div>
+    <div style={{ position: "relative" }}>
       <ul className="ListItemsDisplay">
-        {MenuList.map((item, index) => (
-          <li className="ItemsStylesList" key={index}>
-            {item}
+        {MenuList.map((item) => (
+          <li className="ItemsStylesList" key={item.id}>
+            {item.title}
           </li>
         ))}
+        <li
+          className="ItemsStylesList"
+          onClick={() => setIsOpen(!isOpen)}
+          style={{ cursor: "pointer" }}>
+          Your cart
+        </li>
       </ul>
       <div className="LogoAndInformation">
         <div className="Logo">
@@ -40,6 +47,7 @@ const Menu = () => {
           </ul>
         </div>
       </div>
+      {isOpen && <ShopCard close={setIsOpen} />}
     </div>
   );
 };
